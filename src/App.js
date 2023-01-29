1dee064df16effa758515627fc5390887ad7bed3
dingyi <dingyi@example.com>
2023-01-29 18:04:08 +0800

    day1

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import IndexRouter from './router/IndexRouter'
+export default function App() {
+  return (
+    <div>
+      <h1>App</h1>
+      <IndexRouter>
+
+      </IndexRouter>
+    </div>
+  )
+}
---
+++ src/router/IndexRouter.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {HashRouter,Route,Routes} from 'react-router-dom'
+import Login from '../views/login/Login'
+import NewsSandBox from '../views/sandbox/NewsSandBox'
+export default function IndexRouter() {
+  return (
+    <HashRouter>
+       
+        <Routes>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/'  element={<NewsSandBox></NewsSandBox>}></Route>
+        </Routes>
+    </HashRouter>
+  )
+}
